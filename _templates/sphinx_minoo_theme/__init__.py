4fa54810bb3a869e5fdb359bd2651d10cf397a6f
Sourcery AI <>
2022-08-01 02:15:59 +0000

    'Refactored by Sourcery'

--- _templates/sphinx_minoo_theme/__init__.py
+++ _templates/sphinx_minoo_theme/__init__.py
@@ -13,15 +13,43 @@ class MinooVersion():
     level = "Beta"
     release = r"2015/05/21"
     def info(self):
-        info = ".".join([self.major, self.minor, self.micro])
-        return info
+        return ".".join([self.major, self.minor, self.micro])
     def info_full(self):
-        info = "major   = " + self.major + "\n" + \
-            "minor   = " + self.minor + "\n" + \
-            "micro   = " + self.micro + "\n" + \
-            "level   = " + self.level + "\n" + \
-            "release = " + self.release + "\n"
-        return info
+        return (
+            (
+                (
+                    (
+                        (
+                            (
+                                (
+                                    (
+                                        (
+                                            (
+                                                (
+                                                    f"major   = {self.major}"
+                                                    + "\n"
+                                                    + "minor   = "
+                                                )
+                                                + self.minor
+                                            )
+                                            + "\n"
+                                        )
+                                        + "micro   = "
+                                    )
+                                    + self.micro
+                                )
+                                + "\n"
+                            )
+                            + "level   = "
+                        )
+                        + self.level
+                    )
+                    + "\n"
+                )
+                + "release = "
+            )
+            + self.release
+        ) + "\n"
 
 __version__ = MinooVersion()
 
@@ -29,8 +57,7 @@ __version__ = MinooVersion()
 # From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 def get_html_theme_path():
     """Return list of HTML theme paths."""
-    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    return cur_dir
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
 
